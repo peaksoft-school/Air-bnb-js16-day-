8f5de49cb68d32d7508050138b9c115ac6a26368
Baktur Samatbekovich <Baktur Samatbekovich>
2025-03-21 11:29:12 +0600

    update toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
-const App = () => <h1>Airbnb Front js-16</h1>
+import { showToast } from './utils/helpers/showToast'
+
+const App = () => (
+   <>
+      <button
+         onClick={() =>
+            showToast({
+               title: 'hello',
+               message: 'hi',
+               type: 'error',
+               autoClose: 2000,
+            })
+         }
+      >
+         click
+      </button>
+   </>
+)
 
 export default App
