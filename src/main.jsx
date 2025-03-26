485af638ac4c538caa8267cbf28363a54e131582
Baktur Samatbekovich <Baktur Samatbekovich>
2025-03-26 18:12:21 +0600

    create themes

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Notification from './components/Notification.jsx'
+import Themes from './components/UI/Themes.jsx'
 
 createRoot(document.getElementById('root')).render(
    <StrictMode>
-      <App />
-      <Notification />
+      <Themes>
+         <App />
+         <Notification />
+      </Themes>
    </StrictMode>
 )
