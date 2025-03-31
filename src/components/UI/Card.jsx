3cdb8c7ce2726970d5ddef37ed17542a3ce1353b
Temirlan <bekbosunovtemirlan33@gmail.com>
2025-03-31 13:23:22 +0600

    update card

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import styled from 'styled-components'
 import {
-   Card,
+   Card as MuiCard,
    CardMedia,
    CardContent,
    Typography,
-   Button,
    Box,
    IconButton,
 } from '@mui/material'
@@ -14,67 +17,29 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import Button from './Button'
 
-const CustomCard = ({ imageUrls, price, rating, title, location, guests }) => {
+const Card = ({ imageUrls, price, rating, title, location, guests }) => {
    const [isLiked, setIsLiked] = useState(false)
    const [hovered, setHovered] = useState(false)
 
-   const handleLike = () => {
-      setIsLiked(!isLiked)
-   }
+   const handleLike = () => setIsLiked(!isLiked)
 
-   const CustomPrevArrow = (props) => {
-      const { onClick } = props
-      return (
-         <IconButton
-            sx={{
-               position: 'absolute',
-               top: '50%',
-               left: 10,
-               transform: 'translateY(-50%)',
-               backgroundColor: 'rgba(0, 0, 0, 0.5)',
-               color: '#fff',
-               zIndex: 1,
-               '&:hover': {
-                  backgroundColor: 'rgba(0, 0, 0, 0.7)',
-               },
-            }}
-            onClick={onClick}
-         >
-            <ArrowBackIosNewIcon />
-         </IconButton>
-      )
-   }
+   const CustomPrevArrow = ({ onClick }) => (
+      <StyledIconButtonBack onClick={onClick}>
+         <ArrowBackIosNewIcon />
+      </StyledIconButtonBack>
+   )
 
-   const CustomNextArrow = (props) => {
-      const { onClick } = props
-      return (
-         <IconButton
-            sx={{
-               position: 'absolute',
-               top: '50%',
-               right: 10,
-               transform: 'translateY(-50%)',
-               backgroundColor: 'rgba(0, 0, 0, 0.5)',
-               color: '#fff',
-               zIndex: 1,
-               '&:hover': {
-                  backgroundColor: 'rgba(0, 0, 0, 0.7)',
-               },
-            }}
-            onClick={onClick}
-         >
-            <ArrowForwardIosIcon />
-         </IconButton>
-      )
-   }
+   const CustomNextArrow = ({ onClick }) => (
+      <StyledIconButtonForward onClick={onClick}>
+         <ArrowForwardIosIcon />
+      </StyledIconButtonForward>
+   )
 
    const settings = {
-      dots: true,
-      infinite: true,
+      dots: imageUrls.length > 1,
+      infinite: false,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -84,24 +49,37 @@ const CustomCard = ({ imageUrls, price, rating, title, location, guests }) => {
    }
 
    return (
-      <Card sx={{ maxWidth: 300, margin: 2 }}>
+      <MuiCard sx={{ maxWidth: 300, margin: 2 }}>
          <Box
             sx={{ position: 'relative' }}
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
          >
             <Slider {...settings}>
-               {imageUrls.map((url, index) => (
+               {imageUrls.length > 1 ? (
+                  <Slider {...settings}>
+                     {imageUrls.map((url, index) => (
+                        <div key={index}>
+                           <CardMedia
+                              component="img"
+                              height="140"
+                              image={url}
+                              alt={title}
+                           />
+                        </div>
+                     ))}
+                  </Slider>
+               ) : (
                   <CardMedia
-                     key={index}
                      component="img"
                      height="140"
-                     image={url}
+                     image={imageUrls[0]}
                      alt={title}
                   />
-               ))}
+               )}
             </Slider>
          </Box>
+
          <CardContent>
             <Box
                display="flex"
@@ -111,42 +89,28 @@ const CustomCard = ({ imageUrls, price, rating, title, location, guests }) => {
                <Typography variant="h6" component="div">
                   ${price} <span style={{ color: 'gray' }}>/ day</span>
                </Typography>
-               <Box
-                  sx={{
-                     width: '3em',
-                     height: '25px',
-                     backgroundColor: '#828282',
-                     display: 'flex',
-                     alignItems: 'center',
-                     justifyContent: 'center',
-                     borderRadius: '2px',
-                  }}
-               >
+
+               <StyledRating>
                   <Typography variant="body2" sx={{ color: 'white' }}>
-                     <StarIcon sx={{ color: '#F7D212', fontSize: '13px' }} />{' '}
+                     <StarIcon sx={{ color: '#F7D212', fontSize: '13px' }} />
                      {rating}
                   </Typography>
-               </Box>
+               </StyledRating>
             </Box>
-            <Typography
-               gutterBottom
-               variant="h5"
-               component="div"
-               sx={{
-                  whiteSpace: 'nowrap',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-               }}
-            >
+
+            <StyledTitle gutterBottom variant="h5" component="div">
                {title}
-            </Typography>
+            </StyledTitle>
+
             <Box display="flex" alignItems="center" gap={1}>
                <LocationOnIcon fontSize="small" color="action" />
+
                <Typography variant="body2" color="text.secondary">
                   {location}
                </Typography>
             </Box>
          </CardContent>
+
          <Box
             display="flex"
             justifyContent="space-around"
@@ -158,10 +122,10 @@ const CustomCard = ({ imageUrls, price, rating, title, location, guests }) => {
                   {guests} guests
                </Typography>
             </Box>
+
             <Box display="flex" gap="10px">
-               <Button variant="contained" color="warning">
-                  BOOK
-               </Button>
+               <Button width={100}>BOOK</Button>
+
                <IconButton aria-label="like" onClick={handleLike}>
                   {isLiked ? (
                      <FavoriteIcon color="warning" />
@@ -171,8 +135,52 @@ const CustomCard = ({ imageUrls, price, rating, title, location, guests }) => {
                </IconButton>
             </Box>
          </Box>
-      </Card>
+      </MuiCard>
    )
 }
 
-export default CustomCard
+export default Card
+
+const StyledIconButtonBack = styled(IconButton)(() => ({
+   position: 'absolute',
+   top: '50%',
+   left: 10,
+   transform: 'translateY(-50%)',
+   backgroundColor: 'rgba(0, 0, 0, 0.5)',
+   color: '#fff',
+   zIndex: 1,
+
+   '&:hover': {
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+   },
+}))
+
+const StyledIconButtonForward = styled(IconButton)(() => ({
+   position: 'absolute',
+   top: '50%',
+   right: 10,
+   transform: 'translateY(-50%)',
+   backgroundColor: 'rgba(0, 0, 0, 0.5)',
+   color: '#fff',
+   zIndex: 1,
+
+   '&:hover': {
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+   },
+}))
+
+const StyledRating = styled(Box)(() => ({
+   width: '3em',
+   height: '25px',
+   backgroundColor: '#828282',
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+   borderRadius: '2px',
+}))
+
+const StyledTitle = styled(Typography)(() => ({
+   whiteSpace: 'nowrap',
+   overflow: 'hidden',
+   textOverflow: 'ellipsis',
+}))
