639522f570b21b2c7821e0253dea0cf9ddc9f8f5
Baktur005 <baktur73@gmail.com>
2025-04-01 19:59:45 +0600

    new update chip

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import Chip from './components/UI/Chip'
-
-const App = () => (
-   <>
-      <h1>Airbnb Front js-16</h1>
-      <Chip icon={true} label={'hello'} />
-   </>
-)
-
-export default App
+const App = () => <h1>Airbnb Front js-16</h1>
+ 
+ export default App
