a173f9e4c4033a73339337b949de359c73b02870
Baktur005 <baktur73@gmail.com>
2025-04-02 17:02:41 +0600

    creation bread-crumbs

---
+++ src/utils/constants/routeBreadCrumbs.jsx
@@ -0,0 +1,15 @@
+export const royteBreadCrumbs = {
+   ADMIN: {
+      index: '/admin',
+   },
+
+   USER: {
+      index: '/user',
+   },
+}
+
+export const ROLES = {
+   GUEST: 'GUEST',
+   USER: 'USER',
+   ADMIN: 'ADMIN',
+}
