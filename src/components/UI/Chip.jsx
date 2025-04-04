10dcb416ba5deecd039da1fbe96c75f219581a7e
Baktur005 <baktur73@gmail.com>
2025-04-04 13:19:01 +0600

    update Chip

--- src/components/UI/Chip.jsx
+++ src/components/UI/Chip.jsx
@@ -1,11 +1,11 @@
 import { Chip as MuiChip } from '@mui/material'
 import ClearIcon from '@mui/icons-material/Clear'
-import { styled } from '@mui/material/styles'
+import styled from 'styled-components'
 
-const Chip = ({ icon = false, onClick, onDelete, label }) => {
+const Chip = ({ onClick, onDelete, label }) => {
    return (
       <StyledMuiChip
-         icon={icon && <ClearIcon />}
+         deleteIcon={<ClearIcon />}
          onDelete={onDelete}
          onClick={onClick}
          label={label}
@@ -16,38 +16,43 @@ const Chip = ({ icon = false, onClick, onDelete, label }) => {
 export default Chip
 
 const StyledMuiChip = styled(MuiChip)({
-   width: '95px',
-   height: '35px',
-   borderRadius: '1px',
-   backgroundColor: '#F3F3F3',
-   fontSize: '16px',
-   display: 'flex',
-   justifyContent: 'center',
-   alignItems: 'center',
-   gap: '6px',
+   '&.MuiChip-root': {
+      width: '95px',
+      height: '35px',
+      borderRadius: '1px',
+      backgroundColor: '#F3F3F3',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '9px 8px 9px 8px',
 
-   '&:hover': {
-      backgroundColor: '#C4C4C4',
-   },
+      '&:hover': {
+         backgroundColor: '#C4C4C4',
+      },
 
-   '&:active': {
-      backgroundColor: '#A1A1A1',
-   },
+      '&:active': {
+         backgroundColor: '#A1A1A1',
+      },
 
-   '& .MuiChip-label, & .MuiChip-icon': {
-      fontFamily: 'Arial',
-      fontWeight: 400,
-      fontSize: '16px',
-      lineHeight: '100%',
-      color: '#828282',
-      transition: 'color 0.3s ease',
-   },
+      '& .MuiChip-label, & .MuiSvgIcon-root': {
+         fontFamily: 'Arial',
+         fontWeight: 400,
+         fontSize: '16px',
+         lineHeight: '100%',
+         color: '#828282',
+         transition: 'color 0.3s ease',
+      },
 
-   '&:hover .MuiChip-label, &:hover .MuiChip-icon': {
-      color: '#F7F7F7',
-   },
+      '& .MuiSvgIcon-root': {
+         order: '-1',
+      },
 
-   '&:active .MuiChip-label, &:active .MuiChip-icon': {
-      color: '#F7F7F7',
+      '&:hover .MuiChip-label, &:hover .MuiSvgIcon-root': {
+         color: '#F7F7F7',
+      },
+
+      '&:active .MuiChip-label, &:active .MuiSvgIcon-root': {
+         color: '#F7F7F7',
+      },
    },
 })
