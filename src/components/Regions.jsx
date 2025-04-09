8ce00faf0b023df9851fe2018b19a04790617117
Baktur005 <baktur73@gmail.com>
2025-04-09 14:27:31 +0600

    error removal

--- src/components/Regions.jsx
+++ src/components/Regions.jsx
@@ -1,6 +1,5 @@
 import { Box, styled } from '@mui/material'
-import { color, motion } from 'framer-motion'
-// import styled from 'styled-components'
+import { motion } from 'framer-motion'
 import React from 'react'
 
 import chui from '../assets/images/Chui.png'
