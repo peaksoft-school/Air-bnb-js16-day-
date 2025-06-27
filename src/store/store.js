c90519c1902b2fbb6f90f7fdc76d9222d9b3cb5e
Baktur005 <baktur73@gmail.com>
2025-06-27 12:30:48 +0600

    fix: add authSlice store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import { authReducer } from './slices/auth/authSlice'
 import { userInfoReducer } from './slices/admin/user/userInfoSlice'
 import { addHouseSlice } from './slices/user/addHouse/addHouseSlice'
 import { houseSlice } from './slices/user/house/houseSlice'
@@ -9,13 +8,14 @@ import persistStore from 'redux-persist/es/persistStore'
 import persistReducer from 'redux-persist/es/persistReducer'
 import storage from 'redux-persist/lib/storage'
 import { allHousingSlice } from './slices/admin/all-housing/allHousingSlice'
+import { authSlice } from './slices/auth/authSlice'
 
 const rootReducer = combineReducers({
    userInfo: userInfoReducer,
    addHouseSlice: addHouseSlice.reducer,
    houseSlice: houseSlice.reducer,
    applicationSlice: houseApplicationsReducer,
-   auth: authReducer,
+   [authSlice.name]: authSlice.reducer,
 })
 
 const persistConfig = {
